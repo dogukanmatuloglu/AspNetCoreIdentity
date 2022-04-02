9bd5d044a8389814b67a9a244ed68d783935cce7
dogukan <dogukan.matul@gmail.com>
2022-04-02 23:54:51 +0300

    Üye anasayfası tasarlandı ve kullanıcı bilgileri viewa taşındı

--- WepApp/Controllers/MemberController.cs
+++ WepApp/Controllers/MemberController.cs
@@ -1,18 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WepApp.Models;
+using WepApp.ViewModels;
 
 namespace WepApp.Controllers
 {
     [Authorize]
     public class MemberController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
-            return View();
+          
+            AppUser user = _userManager.FindByNameAsync(HttpContext.User.Identity.Name).Result;
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+            return View(userViewModel);
         }
     }
 }
