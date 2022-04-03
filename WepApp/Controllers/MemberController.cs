964799fc5840a6de1c94c85b5dc3bfe8c0856ad6
dogukan <dogukan.matul@gmail.com>
2022-04-04 00:50:44 +0300

    Üye şifre değiştirme sayfası tasarlandı ve ChangePassword Action ı kodlandı

--- WepApp/Controllers/MemberController.cs
+++ WepApp/Controllers/MemberController.cs
@@ -29,5 +29,49 @@ namespace WepApp.Controllers
             UserViewModel userViewModel = user.Adapt<UserViewModel>();
             return View(userViewModel);
         }
+        public IActionResult PasswordChange()
+        {
+
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PasswordChange(PasswordChangeViewModel passwordChangeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+                if (User!=null)
+                {
+                    bool exist = _userManager.CheckPasswordAsync(user, passwordChangeViewModel.PasswordOld).Result;
+                    if (exist)
+                    {
+                        IdentityResult ıdentityResult = _userManager.ChangePasswordAsync(user, passwordChangeViewModel.PasswordOld, passwordChangeViewModel.PasswordNew).Result;
+                        if (ıdentityResult.Succeeded)
+                        {
+                           
+                            _userManager.UpdateSecurityStampAsync(user);
+                            _signInManager.SignOutAsync();
+                            _signInManager.PasswordSignInAsync(user, passwordChangeViewModel.PasswordNew, true, false);
+                            ViewBag.succes = "true";
+                        }
+                        else
+                        {
+                            foreach (var item in ıdentityResult.Errors)
+                            {
+                                ModelState.AddModelError("", item.Description);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Eski Şifreniz yanlıştır");
+                    }
+                }
+
+            }
+
+            return View();
+        }
     }
 }
---
+++ WepApp/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WepApp.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [Display(Name ="Eski Şifreniz")]
+        [Required(ErrorMessage ="Eski Şifreniz Gereklidir.")]
+        [DataType(dataType:DataType.Password)]
+        [MinLength(4,ErrorMessage ="Şifreniz en az 4 karakter olmalıdır.")]
+        public string PasswordOld { get; set; }
+
+        [Display(Name = "Yeni Şifreniz")]
+        [Required(ErrorMessage = "Yeni Şifreniz Gereklidir.")]
+        [DataType(dataType: DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakter olmalıdır.")]
+        public string PasswordNew { get; set; }
+        [Display(Name = " Onay Yeni Şifreniz")]
+        [Required(ErrorMessage = " Onay Yeni Şifreniz Gereklidir.")]
+        [DataType(dataType: DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakter olmalıdır.")]
+        [Compare("PasswordNew",ErrorMessage ="Yeni şifreniz ve onay şifreniz birbirinden farklıdır.")]
+        public string PsswordConfirm { get; set; }
+    }
+}
