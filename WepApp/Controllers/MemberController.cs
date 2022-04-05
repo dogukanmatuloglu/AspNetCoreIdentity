e89431162a8476cab73ce5bb7cf4db284804d0d4
dogukan <dogukan.matul@gmail.com>
2022-04-05 05:24:35 +0300

    Üye bilgileri güncelleme sayfası tasarlandı ve UserEdit action ı kodlandı

--- WepApp/Controllers/MemberController.cs
+++ WepApp/Controllers/MemberController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using WepApp.Models;
 using WepApp.ViewModels;
 
+
 namespace WepApp.Controllers
 {
     [Authorize]
@@ -27,6 +28,49 @@ namespace WepApp.Controllers
           
             AppUser user = _userManager.FindByNameAsync(HttpContext.User.Identity.Name).Result;
             UserViewModel userViewModel = user.Adapt<UserViewModel>();
+            return View(userViewModel);
+        }
+
+
+        public IActionResult UserEdit()
+        {
+            AppUser user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+
+
+            return View(userViewModel);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UserEdit(UserViewModel userViewModel)
+        {
+            ModelState.Remove("Password");
+            if (ModelState.IsValid)
+            {
+                AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.UserName = userViewModel.UserName;
+                user.PhoneNumber = userViewModel.PhoneNumber;
+                user.Email = userViewModel.Email;
+               IdentityResult result=  await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    
+                  await   _userManager.UpdateSecurityStampAsync(user);
+                   await _signInManager.SignOutAsync();
+                    await _signInManager.SignInAsync(user, true);
+                    ViewBag.succes = "true";
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+
             return View(userViewModel);
         }
         public IActionResult PasswordChange()
