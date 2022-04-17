c3abca77babcdb45104d48ba163ee39a23234fcd
dogukan <dogukan.matul@gmail.com>
2022-04-17 18:13:28 +0300

    Rol oluşturma işlemleri yapıldı

--- WepApp/Controllers/AdminController.cs
+++ WepApp/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WepApp.Models;
+using WepApp.ViewModels;
 
 namespace WepApp.Controllers
 {
@@ -26,6 +27,36 @@ namespace WepApp.Controllers
         {
            var users=_userManager.Users.ToList();
             return View(users);
+        }
+
+
+        public IActionResult RoleCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RoleCreate(RoleViewModel roleViewModel)
+        {
+
+            AppRole role = new AppRole();
+            role.Name = roleViewModel.Name;
+            IdentityResult result = _roleManager.CreateAsync(role).Result;
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Roles");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View(roleViewModel);
+        }
+        public IActionResult Roles()
+        {
+            return View(_roleManager.Roles.ToList());
         }
     }
 }
